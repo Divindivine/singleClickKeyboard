b3c9b3a370e454c74dd46f90d3fe73a9c9f393db
Divindivine <divinparakalayil@gmail.com>
2024-08-13 08:45:06 +0530

    13/08/2024

---
+++ script.js
@@ -0,0 +1,21 @@
+function wordKey(button){
+    if(button.id === 'uppercasebutton'){
+        const characters = document.getElementsByClassName('char');
+        for(let char of characters){
+            char.style.textTransform = 'uppercase';
+        }
+    }
+        
+}
+
+
+
+function senderSelector(){
+    
+}
+
+
+
+
+
+
